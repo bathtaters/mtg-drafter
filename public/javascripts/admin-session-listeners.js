ae0797f7e311163af19070953445604f178a65b4
bathtaters <nomoremrnice7@gmail.com>
2021-04-28 18:00:02 -0400

    show/hide boards

--- public/javascripts/admin-session-listeners.js
+++ public/javascripts/admin-session-listeners.js
@@ -8,7 +8,37 @@ function initListeners() {
     for (var i = 0, l = connectors.length; i < l; i++) {
         addListenerAllBrswrs(connectors[i],"click",disconnectOne);
     }
+
+    // Show/Hide main/side boards
+    var boardHeaders = document.getElementsByClassName("boardHeader");
+    for (var i = 0, l = boardHeaders.length; i < l; i++) {
+        addListenerAllBrswrs(boardHeaders[i],"click",clickHideGroup);
+    }
 }
+
+
+// Click to hide board (Copied from 'draft-listeners')
+function clickHideGroup(e) {
+    stopPropagationAllBrswrs(e);
+    var elem = this || e.target || e.srcElemnt;
+    log("show/hide "+elem.id);
+
+    var prefix = elem.id.slice(0, -("Header".length));
+
+    if (prefix) {
+        var container = document.getElementById(prefix+"Container");
+        var hideText = document.getElementById(prefix+"Hider");
+        if (container.classList.contains("hiddenGroup")) {
+            container.classList.remove("hiddenGroup");
+            hideText.innerText = "–"; // en-dash
+        } else {
+            container.classList.add("hiddenGroup");
+            hideText.innerText = "+";
+        }
+    }
+}
+
+
 
 // Change draft type
 function disconnectAll(e) {
