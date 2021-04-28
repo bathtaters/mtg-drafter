4561e3dc5bab139cc4092c38ada1789a0dd31614
bathtaters <nomoremrnice7@gmail.com>
2021-04-28 17:59:15 -0400

    organize admin panel + deck download

---
+++ routes/admin/session.js
@@ -0,0 +1,113 @@
+// ADMIN PANEL - SESSION DETAILS router
+
+var express = require('express');
+var router = express.Router();
+
+const Draft = require('../../models/Draft');
+const { sessionListData, playerData } = require('../../controllers/shared/populateSession');
+const { addSlash } = require('../../controllers/shared/middleware');
+const { reply, daysAgo } = require('../../controllers/shared/basicUtils');
+const fileOps = require('../../controllers/draft/fileOps');
+
+  
+   
+
+/* ----- GET Session Detail page. ----- */
+
+// Get page
+router.get('/:sessionId', addSlash, async function(req, res, next) {
+    const session = await Draft.findBySessionId(req.params.sessionId);
+    if (!session) return res.send('Session "'+req.params.sessionId+'" does not exist.');
+
+    const players = await playerData(session);
+    return res.render('session', {
+        title: 'Admin - '+session.name,
+        session: sessionListData(session),
+        players: players
+    });
+});
+
+
+
+
+
+
+/* ----- POST session changes. ----- */
+
+// Remove based on sessionId
+router.post('/:sessionId/Delete', async function(req, res, next) {
+    const session = await Draft.findBySessionId(req.params.sessionId,'_id');
+    if (!session) return reply(res, {error:'Session "'+req.params.sessionId+'" does not exist.'});
+    await session.deleteOne();
+    return reply(res, {sessionIds: session.sessionId, action: 'Delete'});
+});
+
+// Remove based on date
+router.post('/:sessionId/Clear', async function(req, res, next) {
+    const sessions = await Draft.find({updatedAt: {$lte: daysAgo(req.body.clearDays)}},'_id')
+    const sessionIds = await Promise.all(sessions.map(async session => {
+      await session.deleteOne(); return session.sessionId;
+    }));
+    
+    
+    return reply(res, {sessionIds, action: 'Clear'});
+});
+
+// Disconnect all players
+router.post('/:sessionId/Disconnect', async function(req, res, next) {
+    const session = await Draft.findBySessionId(req.params.sessionId,'players');
+    if (!session) return reply(res, {error:'Session "'+req.params.sessionId+'" does not exist.'});
+
+    await session.disconnectAll();
+    return reply(res, {sessionId: session.sessionId, disconnected: session.players.every(p => !p.connected)})
+});
+    
+// Disconnect a single player
+router.post('/:sessionId/PlayerDisconnect', async function(req, res, next) {
+    const session = await Draft.findBySessionId(req.params.sessionId,'players');
+    if (!session) return reply(res, {error:'Session "'+req.params.sessionId+'" does not exist.'});
+
+    const player = await session.findPlayerByCookie(req.body.playerId,'connected');
+    if (!player) return reply(res, {
+      sessionId: req.params.sessionId,
+      error:'Player "'+req.body.playerId+'" does not exist.'
+    });
+
+    player.connected = false; await session.save();
+    return reply(res, {sessionId: session.sessionId, playerId: player.cookieId, disconnected: !player.connected})
+});
+
+// Download player deck list
+router.get('/:sessionId/player/:playerId/downloadDeck', async function(req, res, next) {
+    const session = await Draft.findBySessionId(req.params.sessionId,'name players');
+    if (!session) return res.send('Session "'+req.params.sessionId+'" does not exist.');
+    const player = await session.findPlayerByCookie(req.params.playerId,'name cards');
+    if (!player) return res.send('Player "'+req.params.playerId+'" does not exist.');
+
+    const deckText = await fileOps.export(player.cards);
+    const filename = player.name+' - '+session.name+' Deck.txt'
+    res.set({
+        'Content-Type': 'text/plain',
+        'Content-Disposition':'attachment; filename="'+filename+'"'
+    });
+    console.log('ADMIN: downloaded deck list for '+player.cookieId);
+    return res.send(deckText);
+});
+
+
+// Catch other actions
+router.post('/:sessionId/:action', async function(req, res, next) {
+    return reply(res, {sessionId: req.params.sessionId, action: req.params.action, error: 'Invalid action.'});
+});
+  
+
+
+
+// Forward initial session POST to session/id GET
+router.post('/', async function(req, res, next) {
+    if(!req.body.sessionId) return res.send('No sessionId posted.');
+    console.log('Redirecting to: '+req.body.sessionId);
+    res.redirect(req.originalUrl + (req.originalUrl.endsWith('/') ? '' : '/') + req.body.sessionId+'/');
+});
+  
+module.exports = router;
