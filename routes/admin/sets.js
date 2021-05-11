2dd41689d5693276baf8fde5eafafb7167499991
bathtaters <nomoremrnice7@gmail.com>
2021-05-11 03:46:43 -0400

    renamed + reorg'd router files

--- routes/admin/sets.js
+++ routes/admin/sets.js
@@ -7,10 +7,70 @@ const Settings = require('../../models/Settings');
 const setList = require('../../admin/setList');
 const { setDetailLayout, sortedKeys } = require('../../config/definitions');
 const { addSlash } = require('../../controllers/shared/middleware');
+const { reply } = require('../../controllers/shared/basicUtils');
 
 
 
-/* GET setDetail page. */
+/* ----- POST set actions ----- */
+
+// Toggle set visibility
+router.post('/ToggleVisibility', async function(req, res, next) {
+  const checkSet = await Settings.get('defaultSet');
+
+  const results = await Promise.all(
+    req.body.setCodes.map( setCode => {
+      if (checkSet !== setCode) return setList.setVisibility(setCode);
+      console.error('Cannot toggle visibilty of default set: '+setCode);  
+      return Promise.resolve(-2);
+    })
+  );
+  
+  return reply(res, {set: req.body.setCodes, action: 'ToggleVisibility', results});
+});
+
+// Toggle set that starts in picker
+router.post('/MakeDefault', async function(req, res, next) {
+  let result = 'Invalid action.';
+
+  const setData = await setList.getSetData(req.body.setCode);
+  if (!setData.enabled) console.error(req.body.setCode+' is not a set or set is hidden.');
+  else result = await Settings.set('defaultSet',req.body.setCode);
+
+  return reply(res, {set: req.body.setCode || 'all', action: 'MakeDefault', result});
+});
+
+// Append new sets to setList
+router.post('/UpdateAll', async function(req, res, next) {
+  const result = await setList.updateSetList(
+    req.body.defaultVisible === undefined ? true : +req.body.defaultVisible
+  );
+  return reply(res, {set: req.body.setCode || 'all', action: 'UpdateAll', result});
+});
+
+// Reset setList and all visibility
+router.post('/ResetAll', async function(req, res, next) {
+  const result = await setList.resetSetList(
+    req.body.defaultVisible === undefined ? true : +req.body.defaultVisible
+  );
+  return reply(res, {set: req.body.setCode || 'all', action: 'ResetAll', result});
+});
+
+/* Redirect empty POST request as GET. */
+router.post('/', addSlash, function(req, res, next) {
+  console.log('POST to /set w/o action - Redirecting to set/setCode');
+  if (!req.body.sets && !req.body.setCode) return res.send('No set data sent');
+  if (Array.isArray(req.body.sets)) req.body.sets = req.body.sets[0];
+  return res.redirect('./' + (req.body.setCode || req.body.sets) + '/');
+});
+
+
+
+
+
+
+/* ----- GET set detail papges ----- */
+
+// Base setDetail page
 router.get('/:setCode', addSlash, async function(req, res, next) {
     // Lookup Set Data
     let setData = await Set.findById(req.params.setCode,'-sheets');
@@ -38,7 +98,7 @@ router.get('/:setCode', addSlash, async function(req, res, next) {
 
 
 
-/* GET Set/AllCards page. */
+// All cards in set page
 const cardDataObj = ({uuid, name, noGath, gathererImg}) => ({uuid, name, noGath, gathererImg}); 
 router.get('/:setCode/all', addSlash, async function(req, res, next) {
     let setData = await Set.findById(req.params.setCode,'name');
@@ -57,7 +117,7 @@ router.get('/:setCode/all', addSlash, async function(req, res, next) {
 });
 
 
-/* GET Set/Sheet page. */
+// All cards from sheet in set page
 const sheetDataKeys = ['foil', 'balanceColors','totalWeight'];
 router.get('/:setCode/:sheet', addSlash, async function(req, res, next) {
     let setData = await Set.findById(req.params.setCode,'name sheets');
@@ -79,13 +139,5 @@ router.get('/:setCode/:sheet', addSlash, async function(req, res, next) {
       });
 });
 
-
-/* Send POST request to GET. */
-router.post('/', addSlash, function(req, res, next) {
-    console.log('POSTED TO SET');
-    if (!req.body.sets && !req.body.setCode) return res.send('No set data sent');
-    if (Array.isArray(req.body.sets)) req.body.sets = req.body.sets[0];
-    return res.redirect('./' + (req.body.setCode || req.body.sets) + '/');
-});
 
 module.exports = router;
