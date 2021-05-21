5bc8871047e44bb849e5cdd874ce07d643e12bbb
bathtaters <nomoremrnice7@gmail.com>
2021-05-21 05:35:33 -0400

    removed page refreshes in favor of fetches

--- public/javascripts/admin-panel-listeners.js
+++ public/javascripts/admin-panel-listeners.js
@@ -4,7 +4,6 @@ function initListeners() {
     var hiders = document.getElementsByClassName("panelHider");
     for (var i = 0, l = hiders.length; i < l; i++) {
         addListenerAllBrswrs(hiders[i],"click",clickHider);
-        console.log("added listener to: "+hiders[i].id);
     }
 
     // Session listeners
@@ -66,18 +65,31 @@ function clickSession(e) {
     var action = elem.value;
     log("clicked: session."+action);
 
-    var data = {};
+    var data = {}, sessions = [0];
     if (action == "Clear") {
         data.clearDays = document.getElementById("clearDays").value;
+        if (!confirm("Are you sure you wish to delete all drafts older than "+data.clearDays+" days?")) {return;}
+    } else {
+        sessions = getSelectedOptions(document.getElementById("sessionBox"));
     }
-    
-    var sessions = getSelectedOptions(document.getElementById("sessionBox"));
-    updateMultiple(sessions,"session/",action,data).then(function() { location.reload(); });
+
+    updateMultiple(sessions,"session/",action,data).then( function(results) {
+        if (!Array.isArray(results) || results.length == 0) {
+            return log("Error updating session(s): "+action, results.error || "Response from server: "+JSON.stringify(resultsx));
+        }
+
+        // Only refresh session box
+        setButtonStatus(false, ["sessionDetails","sessionDelete","sessionDisconnect"]);
+        return updateServer("draftsList", null, ".", true).then( function(newOpts) {
+            if (newOpts) { document.getElementById("sessionBox").innerHTML = newOpts; }
+            else { log("No data returned"); }
+        });
+    });
 }
 
 // Clicking in multi-selection boxes
 function clickSessionBox(e) {
-    setButtonStatus(this || e.target || e.srcElemnt, ["sessionDetails","sessionDelete","sessionDisconnect"])
+    setButtonStatus(this || e.target || e.srcElemnt, ["sessionDetails","sessionDelete","sessionDisconnect"]);
 }
 function chooseSessionDetail() { document.getElementById("sessionDetails").click(); }
 
@@ -99,12 +111,54 @@ function clickSets(e) {
     } else if (action == "MakeDefault") {
         data.setCode = document.getElementById("setsBox").value;
     } else {
+        loadingSplash();
         data.defaultVisible = document.getElementById("setsDefault").value;
     }
 
-    updateServer(action,data,"set").then( result => {
-        log("Completed "+result.action+" on set: "+result.set);
-        location.reload();
+    updateServer(action,data,"set").then( function(result) {
+        
+        // Just update on MakeDefault
+        if (result.action == "MakeDefault" && result.result && result.set) {
+            var oldDefault = document.getElementsByClassName("setDef")[0];
+            if (oldDefault) { oldDefault.classList.remove("setDef"); }
+            
+            var newDefault = getOption(document.getElementById("setsBox"), result.set);
+            if (newDefault) { newDefault.classList.add("setDef"); }
+            
+        // Just update on ToggleVisibility
+        } else if (result.action == "ToggleVisibility" && result.results && Array.isArray(result.set)) {
+            var setBox = document.getElementById("setsBox");
+            for (var i=0, e=result.set.length; i < e; i++) {
+                if (result.results[i] < 0) { continue; }
+                var setOpt = getOption(setBox, result.set[i]);
+                if (!setOpt) { continue; }
+                if (setOpt.classList.contains("setOff")) {
+                    setOpt.classList.remove("setOff");
+                    setOpt.classList.add("setOn");
+                } else {
+                    setOpt.classList.remove("setOn");
+                    setOpt.classList.add("setOff");
+                }
+            }
+        
+        // Only refresh sets box
+        } else if ((result.action == "UpdateAll" || result.action == "ResetAll") && result.result >= 0) {
+            result.set = (result.result || "no")+" sets"
+            
+            setButtonStatus(false, ["setDetails","setToggle","setDefault"]);
+            return updateServer("setsList", null, ".", true).then( function(newOpts) {
+                if (newOpts) { document.getElementById("setsBox").innerHTML = newOpts; }
+                else { log("No data returned"); }
+                loadingSplash(false);
+            });
+        
+        // Errors
+        } else {
+            loadingSplash(false);
+            return log("Error updating set(s): "+action, result.error || "Response from server: "+JSON.stringify(result));
+        }
+
+        log("Completed "+result.action+" on: "+result.set);
     });
 }
 
@@ -206,8 +260,7 @@ function updateDb(e) {
     }
 
     document.getElementById("dbResult").innerText = "Updating database...";
-    document.getElementsByTagName("body")[0].classList.add("noScroll")
-    document.getElementById("dbLoading").classList.remove("hidden");
+    loadingSplash();
 
     postFormData("updateDb",formData,"db").then( result => {
         alert(result.result || "Database update completed with no message.");
@@ -247,5 +300,17 @@ function clickPword(e) {
 }
 
 // Additional in SU Panel
+
+
+// Turn on/off Loading graphic
+function loadingSplash(enable = true) {
+    if (enable) {
+        document.getElementsByTagName("body")[0].classList.add("noScroll");
+        document.getElementById("dbLoading").classList.remove("hidden");
+    } else {
+        document.getElementsByTagName("body")[0].classList.remove("noScroll");
+        document.getElementById("dbLoading").classList.add("hidden");
+    }
+}
 
 addWindowListenerAllBrswrs("load", initListeners, false);
