83300d1ec8004dfa5abb7f390d53e90bb9ac0819
bathtaters <nomoremrnice7@gmail.com>
2022-10-10 03:18:48 -0400

    fix formatting

--- src/components/base/Loader.tsx
+++ src/components/base/Loader.tsx
@@ -3,12 +3,12 @@ import errorMap, { GenericError } from './services/error.map'
 
 type Props = { data?: any, error?: any, message?: string, children: ReactNode }
 
-const TextWrapper = ({children}: {children: ReactNode}) => <div>{children}</div>
+const TextWrapper = ({children}: {children: ReactNode}) => <div className="py-4 px-6">{children}</div>
 
 export default function Loader({ data, error, message, children }: Props) {
 
-  if (error)   return (<TextWrapper>Error: {error.message}</TextWrapper>)
-  if (!data) return (<TextWrapper>Loading...</TextWrapper>)
+  if (error)   return (<TextWrapper>Error: {error.message || error}</TextWrapper>)
+  if (!data) return (<TextWrapper>Connecting...</TextWrapper>)
   if (typeof data === 'number') return (<TextWrapper>{data in errorMap ? errorMap[data] : GenericError(data)}</TextWrapper>)
   if (message) return (<TextWrapper>{message}</TextWrapper>)
 
