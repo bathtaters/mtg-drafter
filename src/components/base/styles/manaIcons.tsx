4d3812a77d117f657087622a9d7f93cac31b89e6
bathtaters <nomoremrnice7@gmail.com>
2022-11-08 14:13:07 -0500

    add hoverClass, inverting colors on hover

--- src/components/base/styles/manaIcons.tsx
+++ src/components/base/styles/manaIcons.tsx
@@ -4,11 +4,14 @@ export const colorClass: { [key: string]: string } = {
   b: "text-ms bg-msb",
   r: "text-ms bg-msr",
   g: "text-ms bg-msg",
-  // w: "text-amber-900 bg-yellow-100",
-  // u: "text-blue-800 bg-indigo-400",
-  // b: "text-gray-400 bg-slate-800",
-  // r: "text-orange-900 bg-red-400",
-  // g: "text-green-800 bg-emerald-400",
+}
+
+export const hoverClass: { [key: string]: string } = {
+  w: "hover:text-msw hover:bg-ms border-msw",
+  u: "hover:text-msu hover:bg-ms border-msu",
+  b: "hover:text-msb hover:bg-ms border-msb",
+  r: "hover:text-msr hover:bg-ms border-msr",
+  g: "hover:text-msg hover:bg-ms border-msg",
 }
 
 export const colorPip: { [key: string]: string } = {
