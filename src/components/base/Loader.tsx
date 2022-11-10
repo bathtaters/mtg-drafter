78fde6ca0223e68a6fc39b2738ad96d198a26eb4
bathtaters <nomoremrnice7@gmail.com>
2022-11-10 02:15:30 -0500

    allow for non-nullish falsy data types

--- src/components/base/Loader.tsx
+++ src/components/base/Loader.tsx
@@ -8,7 +8,7 @@ const TextWrapper = ({children}: {children: ReactNode}) => <div className="py-4 
 export default function Loader({ data, error, message, children }: Props) {
 
   if (error)   return (<TextWrapper>Error: {error.message || error}</TextWrapper>)
-  if (!data) return (<TextWrapper>Connecting...</TextWrapper>)
+  if (data == null) return (<TextWrapper>Connecting...</TextWrapper>)
   if (typeof data === 'number') return (<TextWrapper>{data in errorMap ? errorMap[data] : GenericError(data)}</TextWrapper>)
   if (message) return (<TextWrapper>{message}</TextWrapper>)
 
