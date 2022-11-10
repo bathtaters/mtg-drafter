7ffdb791f146ef009f9ea99e845e0453c3ed5902
bathtaters <nomoremrnice7@gmail.com>
2022-11-10 02:17:12 -0500

    use TextEditor and change Host Dot to Symbol

--- src/components/game/PlayerContainers/PlayerContainerStyle.tsx
+++ src/components/game/PlayerContainers/PlayerContainerStyle.tsx
@@ -1,24 +1,13 @@
-import type { HTMLProps, ReactNode } from "react"
+import type { ReactNode } from "react"
+import TextEditor, { Props as TextEditorProps } from "components/base/common/FormElements/TextEditor"
 
 export const EmptyPlayerContainer = () => <div className="h-20" />
 
-export const HostBadge = () => <span className="badge badge-info badge-sm align-top ml-2">Host</span>
+const HostMarker = () => <span className="-mx-0.5 mb-1 text-lg opacity-70"><div className="ms ms-planeswalker" /></span>
 
-const TitleDot = ({ isMini }: { isMini?: boolean }) => isMini ?
-  <span className="w-0 -ml-1 text-2xs align-top opacity-60">•</span> :
-  <span className="w-2 -ml-3 pr-3 text-xs align-middle opacity-60">•</span>
+export const HostBadge = () => <span className="badge badge-info badge-sm align-top ml-2"><HostMarker />&nbsp;Host</span>
 
-
-// Player Name Editor
-export const PlayerNameWrapper = (props: HTMLProps<HTMLDivElement>) => <div className="h-8" {...props} />
-export const PlayerNameEditWrapper = (props: HTMLProps<HTMLDivElement>) => <div className="input-group h-8 w-auto" {...props} />
-export const PlayerNameEditBtn = (props: HTMLProps<HTMLInputElement>) => (
-  <input type="button" {...props} className={`btn btn-square ${props.className} btn-xs h-auto`} />
-)
-export const PlayerNameTextBox = (props: HTMLProps<HTMLInputElement>) => (
-  <input type="text" className="input input-primary h-auto p-2 text-lg sm:text-2xl w-full" {...props} />
-)
-
+export const PlayerNameEditor = (props: TextEditorProps) => <TextEditor {...props} className="input-primary text-lg sm:text-2xl " />
 
 const themes = {
   self: [
@@ -37,8 +26,8 @@ const themes = {
 }
 
 export default function PlayerContainerStyle(
-  { title, header, subtitle, children, isMini, showDot, disconnected, color }:
-  { title: ReactNode, header?: ReactNode, subtitle?: ReactNode, children?: ReactNode, isMini?: boolean, showDot?: boolean, disconnected?: boolean, color?: ColorTheme }
+  { title, header, subtitle, children, isMini, isHost, disconnected, color }:
+  { title: ReactNode, header?: ReactNode, subtitle?: ReactNode, children?: ReactNode, isMini?: boolean, isHost?: boolean, disconnected?: boolean, color?: ColorTheme }
 ) {
   return (
     <div className={`stats shadow-sm shadow-black ${isMini ? 'rounded-lg' : 'overflow-visible'} ${color ? themes[color][3] : ''}`}>
@@ -53,12 +42,12 @@ export default function PlayerContainerStyle(
         {header && <div className="stat-title text-xs sm:text-base">{header}</div>}
         
         <div className={`stat-value ${
-          isMini ? 'row-span-3 self-center text-base' : 'flex items-center'
+          isMini ? 'row-span-3 self-center text-base flex items-center' : 'flex items-center'
         } ${
           disconnected ? "opacity-60 italic" : ''} ${color ? themes[color][1] : ''
         }`}>
-          {showDot && <TitleDot isMini={isMini} />}
-          <span className={isMini ? "ml-1 flex-grow" : "text-lg sm:text-2xl flex-grow"}>{title}</span>
+          {isMini && isHost && <HostMarker />}
+          <span className={isMini ? "ml-1 flex-grow" : "text-lg sm:text-2xl flex-grow -ml-2 mb-1"}>{title}</span>
         </div>
 
         {subtitle && <div className={`stat-desc ${color ? themes[color][2] : ''}`}>{subtitle}</div>}
