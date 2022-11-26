4d04f082e649e5608a32b5f5ed172113e805eea8
bathtaters <nomoremrnice7@gmail.com>
2022-11-25 22:45:15 -0500

    Add icons to player menu

--- src/components/game/PlayerContainers/PlayerMenu.tsx
+++ src/components/game/PlayerContainers/PlayerMenu.tsx
@@ -1,4 +1,5 @@
 import DropdownMenu from "components/base/common/DropdownMenu"
+import { ExitIcon, ExportIcon, LandIcon, RenameIcon, ToolsIcon } from "components/svgs/MenuIcons"
 import { MenuItemStyle } from "./PlayerContainerElemStyles"
 
 type Props = {
@@ -12,11 +13,13 @@ type Props = {
 export default function PlayerMenu({ saveDeck, openLands, editName, openHost, dropPlayer }: Props) {
   return (
     <DropdownMenu label="☰">
-      {!!saveDeck   && <li><a onClick={saveDeck  }><MenuItemStyle label="Export Deck" /></a></li> }
-      {!!openLands  && <li><a onClick={openLands }><MenuItemStyle label="Set Lands"   /></a></li> }
-      {<li className={editName ? "" : "disabled"}><a onClick={editName}><MenuItemStyle label="Edit Name" /></a></li> }
-      {!!openHost   && <li><a onClick={openHost  }><MenuItemStyle label="Host Tools"  /></a></li> }
-      {!!dropPlayer && <li><a onClick={dropPlayer}><MenuItemStyle label="Drop Game"   /></a></li> }
+      {!!saveDeck   && <li><a onClick={saveDeck  }><MenuItemStyle label="Export Deck" icon={<ExportIcon />} /></a></li> }
+      {!!openLands  && <li><a onClick={openLands }><MenuItemStyle label="Set Lands"   icon={<LandIcon   />} /></a></li> }
+      {<li className={editName ? "" : "disabled"}>
+        <a onClick={editName}><MenuItemStyle label="Edit Name" icon={<RenameIcon />} /></a>
+      </li> }
+      {!!openHost   && <li><a onClick={openHost  }><MenuItemStyle label="Host Tools"  icon={<ToolsIcon  />} /></a></li> }
+      {!!dropPlayer && <li><a onClick={dropPlayer}><MenuItemStyle label="Drop Game"   icon={<ExitIcon   />} /></a></li> }
     </DropdownMenu>
   )
 }
