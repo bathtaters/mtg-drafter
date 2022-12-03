a5d8b88b674115f2d8f2e4f2df69485fbd7f2a68
bathtaters <nomoremrnice7@gmail.com>
2022-12-03 00:40:20 -0500

    clean up and improve typing

--- src/components/base/common/FormElements/IconToggle.tsx
+++ src/components/base/common/FormElements/IconToggle.tsx
@@ -1,17 +1,19 @@
 import type { ReactNode } from "react"
 
-type Props = {
+export type Props = {
   label?: string,
   value: boolean,
   className?: string,
-  children: ReactNode[],
+  children: [ReactNode, ReactNode],
   setValue: (val: boolean) => void
 }
 
 
-export default function IconToggle({ label, value, setValue, className, children }: Props) {
+export default function IconToggle({ label, value, setValue, className = '', children }: Props) {
   return (
-    <label className={`swap${value ? ' swap-active' : ''}${label ? ' tooltip' : ''} tooltip-secondary inline-grid ${className || ''}`} data-tip={label}>
+    <label data-tip={label} className={
+      `swap${value ? ' swap-active' : ''}${label ? ' tooltip' : ''} tooltip-secondary inline-grid ${className}`
+    }>
       <input type="checkbox" checked={value} onChange={(ev) => setValue(ev.target.checked)} />
       <div className="swap-on">{children[0]}</div>
       <div className="swap-off">{children[1]}</div>
