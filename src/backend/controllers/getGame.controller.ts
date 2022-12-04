d86811bc0fff9ff8ed66a51755533600c9588052
bathtaters <nomoremrnice7@gmail.com>
2022-12-04 15:04:30 -0500

    protect game API against unknown users

--- src/backend/controllers/getGame.controller.ts
+++ src/backend/controllers/getGame.controller.ts
@@ -4,14 +4,15 @@ import { getGame } from '../services/game/game.services'
 import { getPlayer } from '../services/game/player.services'
 import { getCtxSessionId, getReqSessionId } from 'components/base/services/sessionId.services'
 
-async function getGameProps(url: string | string[] | undefined, sessionId: string): Promise<ServerProps> {
+async function getGameProps(url: string | string[] | undefined, sessionId: string, throwOnNoPlayer: boolean = false): Promise<ServerProps> {
   if (!url || Array.isArray(url)) return { error: `Invalid URL: ${url}` }
   
   const game = await getGame(url)
   if (!game) return { error: 'Unable to find game' }
   
   const { players, packs, ...options } = game
-  const player = await getPlayer(sessionId, players) as PlayerFull | null // fix for BasicLands <-> JSON value
+  const player = await getPlayer(sessionId, players) as PlayerFull | null // Convert type JSON value -> BasicLands
+  if (throwOnNoPlayer && !player) return { error: 'You are not registered for this game' }
 
   const playerSlots = players.filter(({ sessionId }) => !sessionId).map(({ id }) => id)
   return { options, players, packs, player, playerSlots, sessionId }
@@ -22,8 +23,11 @@ export function serverSideHandler(ctx: GetServerSidePropsContext) {
 }
 
 export async function apiHandler(req: NextApiRequest, res: NextApiResponse<ServerSuccess>) {
-  const props = await getGameProps(req.query.url, getReqSessionId(req, res))
-  if (props.error) return res.status(400).end()
+  const props = await getGameProps(req.query.url, getReqSessionId(req, res), true)
+  if (props.error) {
+    console.error('Error with game',req.query.url,'player',getReqSessionId(req,res),props.error)
+    return res.status(400).end()
+  }
 
   res.status(200).json(props as ServerSuccess)
 }
