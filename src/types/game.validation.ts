8dc5c0682ed0497816a0a3dd3f5fdff68c0b62bf
bathtaters <nomoremrnice7@gmail.com>
2022-12-05 03:18:23 -0500

    add sessionId validation

--- src/types/game.validation.ts
+++ src/types/game.validation.ts
@@ -6,13 +6,14 @@ import { setupLimits, urlLength } from "assets/constants"
 export const boardLands = z.object(fillAndLowerCaseObject(Color, z.number().nonnegative().int()))
 
 const gameData = {
-  url:    nanoId(urlLength),
-  id:     z.string().cuid(),
-  name:   commonOptions.name,
-  round:  z.number().int().nonnegative().lte(setupLimits.packs.max + 1),
-  status: z.nativeEnum(PlayerStatus),
-  board:  z.nativeEnum(Board),
-  basics: z.object(fillAndLowerCaseObject(Board, boardLands)),
+  session: nanoId(),
+  url:     nanoId(urlLength),
+  id:      z.string().cuid(),
+  name:    commonOptions.shape.name,
+  round:   z.number().int().nonnegative().lte(setupLimits.packs.max + 1),
+  status:  z.nativeEnum(PlayerStatus),
+  board:   z.nativeEnum(Board),
+  basics:  z.object(fillAndLowerCaseObject(Board, boardLands)),
 }
 
 export default gameData
