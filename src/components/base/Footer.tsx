b71a8e872a9a785c28dda924dad04b172b2f6fec
bathtaters <nomoremrnice7@gmail.com>
2022-12-06 21:00:05 -0500

    add version/deisclaimers to footer

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -1,15 +1,36 @@
-import { FooterWrapperStyle } from "./styles/AppStyles";
+import { FooterWrapperStyle } from "./styles/AppStyles"
+import pkg from "../../../package.json"
+
 
 export default function Footer() {
   return (
     <FooterWrapperStyle>
-      <a
-        className="link link-primary link-hover"
-        href="https://github.com/bathtaters/mtg-drafter"
-        target="_blank" rel="noopener noreferrer"
-      >
-        GitHub Repo
-      </a>
+      <div className="flex justify-center gap-2">
+        <span>MtG Drafter v{pkg.version}</span>
+        <span className="">|</span>
+        <a
+          className="link link-primary link-hover"
+          href="https://github.com/bathtaters/mtg-drafter"
+          target="_blank" rel="noopener noreferrer"
+        >
+          GitHub Repo
+        </a>
+      </div>
+      <div className="text-2xs leading-tight font-thin opacity-80 italic">
+        <p>
+          This is unofficial Fan Content permitted under the&nbsp;
+          <a
+            className="link link-hover"
+            href="https://github.com/bathtaters/mtg-drafter"
+            target="_blank" rel="noopener noreferrer"
+          >Fan Content Policy</a>.
+        </p><p>
+          It is not approved/endorsed by Wizards.
+          Portions of the materials used are property of Wizards of the Coast
+        </p><p>
+          We only use strictly necessary cookies and do not share any data with third parties.
+        </p>
+      </div>
     </FooterWrapperStyle>
   )
 }
