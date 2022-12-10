b490265e512be25789ba12fc6b80f6347c474649
bathtaters <nomoremrnice7@gmail.com>
2022-12-09 21:41:25 -0500

    fix pre/post game neighborIdx & even round packIdx

--- src/components/game/shared/game.utils.ts
+++ src/components/game/shared/game.utils.ts
@@ -1,5 +1,6 @@
 import type { Game, Player, GameCard, Board, GameStatus } from "@prisma/client"
 import type { PackFull } from "types/game"
+import { mod } from "components/base/services/common.services"
 
 export const getBoard = <C extends GameCard>(playerCards: C[], board: Board) => playerCards.filter(({ board: cardBoard }) => board === cardBoard)
 
@@ -18,14 +19,21 @@ export const passingRight = ({ round, roundCount }: Partial<Game>) =>
   typeof round !== 'number' || round < 1 || round > (roundCount || 0) ? undefined :
     round % 2 === 0
 
-export const getPlayerIdx = (players: Player[], player?: Player | null) => !player?.id ? -1 : players.findIndex(({ id }) => id === player.id)
+export const getPlayerIdx = (players: Pick<Player,"id">[], player?: Pick<Player,"id"> | null) => !player?.id ? -1 :
+  players.findIndex(({ id }) => id === player.id)
 
-export const getNeighborIdx = (game: Partial<Game> | undefined, playerCount: number, playerIdx: number, invert = false) =>
-  !game || playerIdx === -1 || playerCount <= 1 ? -1 : passingRight(game) === !invert ?
-    (playerIdx - 1 + playerCount) % playerCount :
+export const getNeighborIdx = (game: Partial<Game> | undefined, playerCount: number, playerIdx: number, invert = false) => {
+  if (!game || playerIdx === -1 || playerCount <= 1) return -1
+  
+  const passRight = passingRight(game)
+  if (typeof passRight !== 'boolean') return -1
+
+  return passRight === invert ?
+    mod(playerIdx - 1, playerCount) :
     (playerIdx + 1) % playerCount
+}
 
-export const getPackIdx = (game: Game | undefined, players: Player[], player: Player | null, offset: number = 0) => {
+export const getPackIdx = (game: Pick<Game,"round"|"roundCount"> | undefined, players: Pick<Player,"id"|"pick">[], player: Pick<Player,"id"> | null) => {
   if (!game || game.round < 1 || game.round > game.roundCount) return -1
   
   const playerIdx = getPlayerIdx(players, player)
@@ -34,7 +42,10 @@ export const getPackIdx = (game: Game | undefined, players: Player[], player: Pl
   const neighborIdx = getNeighborIdx(game, players.length, playerIdx)
   if (neighborIdx !== -1 && players[playerIdx].pick > players[neighborIdx].pick) return -1
 
-  return (game.round - 1) * players.length + (playerIdx + players[playerIdx].pick + offset) % players.length
+  return (game.round - 1) * players.length + mod(
+    (playerIdx + (players[playerIdx].pick - 1) * (passingRight(game) ? -1 : 1)),
+    players.length
+  )
 }
 
 export const getHolding = (players: Player[], game?: Partial<Game>) => !game || !players.length ? [] :
