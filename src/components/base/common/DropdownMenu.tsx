dfb80bea46a52f5b780078098246bc8feab41ad6
bathtaters <nomoremrnice7@gmail.com>
2022-12-15 22:04:14 -0500

    allow programmatic open/close of dropdown menu

--- src/components/base/common/DropdownMenu.tsx
+++ src/components/base/common/DropdownMenu.tsx
@@ -1,20 +1,20 @@
 import type { ReactNode } from "react";
 
-type Props = { label?: ReactNode, labelClass?: string, menuClass?: string, children: ReactNode }
+type Props = { label?: ReactNode, labelClass?: string, menuClass?: string, forceOpen?: boolean, children: ReactNode }
 
 export default function DropdownMenu({
   label = '↓',
   labelClass = 'btn-primary btn-circle btn-md md:w-12 text-lg',
   menuClass = 'p-2 shadow-lg shadow-black bg-base-100 rounded-box w-48 md:w-52',
-  children
+  forceOpen, children
 }: Props) {
   return (
-    <div className="dropdown dropdown-hover dropdown-end">
+    <div className={`dropdown dropdown-end ${typeof forceOpen !== 'boolean' ? 'dropdown-hover' : forceOpen ? 'dropdown-open' : ''}`}>
       <label tabIndex={0} className={`btn ${labelClass}`}>
         {label}
       </label>
 
-      <ul tabIndex={0} className={`dropdown-content menu ${menuClass}`}>
+      <ul tabIndex={0} className={`dropdown-content menu ${menuClass}${forceOpen === false ? ' hidden' : ''}`}>
         {children}
       </ul>
     </div>
