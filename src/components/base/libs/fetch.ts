a07621b8755ca8c1bb6d9cdd8844db925248dfcd
bathtaters <nomoremrnice7@gmail.com>
2022-12-25 01:50:56 -0500

    remove redundant code

--- src/components/base/libs/fetch.ts
+++ src/components/base/libs/fetch.ts
@@ -2,8 +2,7 @@ export const fetcher = <T = any>(input: RequestInfo | URL, init?: RequestInit): 
   fetch(input, init).then((res) => res.status === 200 ? res.json() : res.status)
 
 export const post = <T = any>(url: string, data: any): Promise<T | number> => 
-  fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(data) })
-    .then((res) => res.status === 200 ? res.json() : res.status)
+  fetcher(url, { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(data) })
 
 export const upload = <T = any>(url: RequestInfo | URL, file: File, name: string = 'file'): Promise<T | number> => {
   const data = new FormData()
