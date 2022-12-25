c64cea65ec9dce88db0c0fcb5ed50549228f68c5
bathtaters <nomoremrnice7@gmail.com>
2022-12-25 00:01:24 -0500

    add MTG JSON + logos to footer

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -1,21 +1,30 @@
 import { FooterWrapperStyle } from "./styles/AppStyles"
 import pkg from "../../../package.json"
+import { GitHubLogo, MtgJsonLogo } from "components/svgs/FooterLogos"
+import Link from "next/link"
 
 
 export default function Footer() {
   return (
     <FooterWrapperStyle>
-      <div className="flex justify-center gap-2">
-        <span>MtG Drafter v{pkg.version}</span>
-        <span className="">|</span>
+      <div className="flex justify-center items-center gap-2">
+        <Link href="/" className="link link-primary link-hover">MtG Drafter v{pkg.version}</Link>
+        <span className="opacity-80">|</span>
         <a
-          className="link link-primary link-hover"
+          className="inline-flex items-center link link-primary link-hover"
           href="https://github.com/bathtaters/mtg-drafter"
           target="_blank" rel="noopener noreferrer"
         >
-          GitHub Repo
+          <GitHubLogo className="w-6 h-6 fill-current" />
+          <p className="ml-2">GitHub Repo</p>
+        </a>
+        <span className="opacity-80">|</span>
+        <a href="https://mtgjson.com" className="inline-flex items-center link link-primary link-hover">
+          <MtgJsonLogo className="w-8 fill-current" />
+          <p className="ml-2">Powered by MTGJSON</p>
         </a>
       </div>
+
       <div className="text-2xs leading-tight font-thin opacity-80 italic">
         <p>
           This is unofficial Fan Content permitted under the&nbsp;
