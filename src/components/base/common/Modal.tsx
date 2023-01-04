a4e2935c4dbb0d2ad3cba50d05a078b5561791a5
bathtaters <nomoremrnice7@gmail.com>
2023-01-03 23:38:24 -0500

    allow editing inner classes & hide body scrollbar

--- src/components/base/common/Modal.tsx
+++ src/components/base/common/Modal.tsx
@@ -5,18 +5,21 @@ type Props = {
   setOpen?: Dispatch<SetStateAction<boolean>>,
   title?: ReactNode,
   className?: string,
+  bodyClass?: string,
+  wrapperClass?: string,
   children?: ReactNode,
   buttons?: ReactNode,
 }
 
-export default function ModalWrapper({ title, className, children, buttons, isOpen, setOpen }: Props) {
+export default function ModalWrapper({ title, className, children, buttons, isOpen, setOpen, wrapperClass, bodyClass }: Props) {
   return (
-    <div className={`modal modal-bottom md:modal-middle${isOpen ? ' modal-open' : ''}`} onClick={setOpen ? () => setOpen((st) => !st) : undefined}>
+    <div className={`modal modal-bottom md:modal-middle${isOpen ? ' modal-open' : ''} ${wrapperClass || ''}`} onClick={setOpen ? () => setOpen((st) => !st) : undefined}>
       <div className={`modal-box p-4 md:p-6 ${className || ''}`} onClick={(ev) => ev.stopPropagation()}>
         {title && <h2 className="font-serif mb-6">{title}</h2>}
-        {children && <div className="m-0 md:m-2">{children}</div>}
+        {children && <div className={`m-0 md:m-2 flex-grow ${bodyClass || ''}`}>{children}</div>}
         {buttons && <div className="modal-action">{buttons}</div>}
       </div>
+      {isOpen && <style jsx global>{"html,body,#__next { overflow-y: hidden; }"}</style>}
     </div>
   )
 }
