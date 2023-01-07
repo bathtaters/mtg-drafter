543dcfd852347c1ca86e867d86bdbb2b1d47d8af
bathtaters <nomoremrnice7@gmail.com>
2023-01-07 05:34:03 -0500

    allow inserts to spliceInPlace

--- src/components/base/services/common.services.ts
+++ src/components/base/services/common.services.ts
@@ -18,10 +18,10 @@ export const updateArrayIdx = <T>(array: T[], find: ((entry: T, idx: number) => 
   return array.map((entry,i) => i !== idx ? entry : newVal(entry, i))
 }
 
-export const spliceInPlace = <T>(array: T[], find: ((entry: T, idx: number) => boolean) | number, removeCount: number = 1) => {
+export const spliceInPlace = <T>(array: T[], find: ((entry: T, idx: number) => boolean) | number, removeCount: number = 1, ...insert: T[]) => {
   const idx = typeof find === 'number' ? find : array.findIndex(find)
   if (idx === -1) return array
-  return array.slice(0, idx).concat(array.slice(idx + removeCount))
+  return array.slice(0, idx).concat(insert).concat(array.slice(idx + removeCount))
 }
 
 export const sameValueObject = <T extends { [key: string]: any }> (keys: (keyof T)[], value: T[keyof T]): T =>
