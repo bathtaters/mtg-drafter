6dc78ed3eebe48d8d87bdb154f01f4c0ebe7d88f
bathtaters <nomoremrnice7@gmail.com>
2023-01-19 00:32:06 -0500

    fix '10 Prisma Clients are already running' error

--- src/backend/libs/db.ts
+++ src/backend/libs/db.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+declare global { var db: PrismaClient | undefined }
 
-const prisma = new PrismaClient()
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production')
+  prisma = new PrismaClient()
+
+// For hot loading in dev env (Prevent multiple instances)
+else {
+  if (!global.db) global.db = new PrismaClient()
+  prisma = global.db
+}
+
 export default prisma
