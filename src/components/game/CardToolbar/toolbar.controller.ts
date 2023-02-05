67441fae34e37aea53f1fccbc4952d670d4d33a0
bathtaters <nomoremrnice7@gmail.com>
2023-02-05 03:31:25 -0500

    remove useLayoutEffect and improve autozooming

--- src/components/game/CardToolbar/toolbar.controller.ts
+++ src/components/game/CardToolbar/toolbar.controller.ts
@@ -1,32 +1,40 @@
 import type { CardOptions } from "types/game"
 import type { AlertsReturn } from "components/base/common/Alerts/alerts.hook"
-import { Dispatch, SetStateAction, useEffect, useLayoutEffect, useState } from "react"
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef } from "react"
 import { useLocalStorage } from "components/base/libs/storage"
 import { sortKeys } from "components/base/services/cardSort.services"
 import cardZoomLevels from "./cardZoomLevels"
 import { zoomToPixels, warn } from "./toolbar.utils"
+import { storageDefaults } from "assets/constants"
 
 export type ToolbarProps = { setCardOptions: Dispatch<SetStateAction<CardOptions>>, notify: AlertsReturn['newToast'], clickReload?: () => void }
 
+const getMaxZoom = () => {
+  let z = cardZoomLevels.length
+  if (typeof window === 'undefined') return z
+
+  while (--z && (zoomToPixels(z) || window.innerWidth) > window.innerWidth) {}
+  return z
+}
+
 export default function useToolbar({ setCardOptions, notify }: ToolbarProps) {
-  
+
+  const maxZoom = useRef(getMaxZoom())
   const [ art,  setArt  ] = useLocalStorage<boolean>('showArt')
   const [ sort, setSort ] = useLocalStorage<number>('sortBy')
-  const [ zoom, setZoom, setTempZoom ] = useLocalStorage<number>('zoom')
+  const [ zoom, setZoomValue, setTempZoom ] = useLocalStorage<number>('zoom')
 
-  useLayoutEffect(() => {
-    const limitWidth = () => {
-      if (typeof window === 'undefined' || window.innerWidth > (zoomToPixels(zoom) || 0)) return false
+  const updateZoom = useCallback((v: string | number) => {
+    if ((v = +v) <= maxZoom.current) return setZoomValue(v)
+    warn('Zoom value limited by window width', notify)
+    setZoomValue(maxZoom.current)
+  }, [notify, setZoomValue])
 
-      let z = zoom || 1
-      while (--z && (zoomToPixels(z) || window.innerWidth) > window.innerWidth) {}
-      setTempZoom(z)
-      return true
-    }
-    if (limitWidth()) warn('Zoom value limited by window width', notify)
+  useEffect(() => {
+    const updateMax = () => (maxZoom.current = getMaxZoom()) < zoom && setTempZoom(maxZoom.current)
 
-    window.addEventListener('resize', limitWidth)
-    return () => window.removeEventListener('resize', limitWidth)
+    window.addEventListener('resize', updateMax)
+    return () => window.removeEventListener('resize', updateMax)
   }, [zoom, setTempZoom])
 
   useEffect(() => { setCardOptions((opt) => ({ ...opt, showArt: art })) }, [art])
@@ -35,6 +43,6 @@ export default function useToolbar({ setCardOptions, notify }: ToolbarProps) {
 
   return {
     art, sort, zoom,
-    setArt, setSort, setZoom: (v: string) => setZoom(+v),
+    setArt, setSort, setZoom: updateZoom,
   }
 }
