04f64149c495f23ce56ca0139e4288a275f352bb
bathtaters <nomoremrnice7@gmail.com>
2023-03-05 17:30:08 -0500

    fix buttons type

--- src/components/game/LogModal/LogToolbar/LogFilter.tsx
+++ src/components/game/LogModal/LogToolbar/LogFilter.tsx
@@ -4,7 +4,7 @@ import { FilterWrapper, FilterButton } from "./LogToolbarStyles"
 
 type Props<ID> = {
   label: string,
-  buttons: { id: ID, name?: string }[],
+  buttons: { id: ID, name?: string | null }[],
   baseList: ID[],
   offset?: number,
   invert?: boolean,
