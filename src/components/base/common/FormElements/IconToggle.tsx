4ddeb2535da6726346030f30b2d27c4c340e5394
bathtaters <nomoremrnice7@gmail.com>
2023-03-08 06:29:07 -0500

    disable toggle while change is loading

--- src/components/base/common/FormElements/IconToggle.tsx
+++ src/components/base/common/FormElements/IconToggle.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react"
+import { ChangeEventHandler, ReactNode, useCallback, useEffect, useState } from "react"
 
 export type Props = {
   label?: string,
@@ -10,11 +10,22 @@ export type Props = {
 
 
 export default function IconToggle({ label, value, setValue, className = '', children }: Props) {
+  const [ loadingState, setLoading ] = useState<boolean>()
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback((ev) => {
+    setLoading(ev.target.checked)
+    setValue(ev.target.checked)
+  }, [setValue])
+
+  useEffect(() => setLoading(undefined), [value])
+
   return (
     <label data-tip={label} className={
-      `swap${value ? ' swap-active' : ''}${label ? ' tooltip' : ''} tooltip-secondary inline-grid ${className}`
+      `swap${value ? ' swap-active' : ''}${label ? ' tooltip' : ''}${
+        typeof loadingState === 'boolean' ? ' btn-disabled cursor-wait pointer-events-auto' : ''
+      } tooltip-secondary inline-grid ${className}`
     }>
-      <input type="checkbox" checked={value} onChange={(ev) => setValue(ev.target.checked)} />
+      <input type="checkbox" checked={value} onChange={handleChange} />
       <div className="swap-on">{children[0]}</div>
       <div className="swap-off">{children[1]}</div>
     </label>
