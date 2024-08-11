0b187772248f2735fa76c62ece6c606d8b99365c
Nick C <bathtaters@gmail.com>
2024-08-10 21:38:33 -0400

    fix import error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import updateImages from '../src/backend/services/db/updateImages'
 import updateSets from '../src/backend/services/db/updateSets'
 import { updateVersion } from 'backend/services/db/updateSettings'
 import { cardDbUrl, imageDbUrl, preferredDbUrl, setsDbUrl } from '../src/assets/urls'
-import { version } from "../package.json"
+import pkg from "../package.json"
 
 const CONSOLE_LOGGING = true
 const FULL_REBUILD = false
@@ -13,7 +13,7 @@ async function main() {
   await updateCards(cardDbUrl, FULL_REBUILD, CONSOLE_LOGGING)
   await updateImages(imageDbUrl, preferredDbUrl, FULL_REBUILD, CONSOLE_LOGGING)
   await updateSets(setsDbUrl, FULL_REBUILD, CONSOLE_LOGGING)
-  await updateVersion(version, CONSOLE_LOGGING)
+  await updateVersion(pkg.version, CONSOLE_LOGGING)
   console.log('DONE')
 }
 
