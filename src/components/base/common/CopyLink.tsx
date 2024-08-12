2bba1fa725331ed044934b13489fe58d99d91215
Nick C <bathtaters@gmail.com>
2024-08-12 04:48:48 -0400

    fix showing link when it is invalid

--- src/components/base/common/CopyLink.tsx
+++ src/components/base/common/CopyLink.tsx
@@ -16,12 +16,12 @@ export type Props = {
 
 export default function CopyLink({ url, message, title, notify, tooltip = "Copy Link", className = "", iconClass = "w-4" }: Props) {
   const [shareable, setCanShare] = useState(false)
-  useEffect(() => { setCanShare(canShare()) }, [url]) // Needed for server-side hydration
+  useEffect(() => { setCanShare(!!url && canShare()) }, [url]) // Needed for server-side hydration
 
-  const handleShare = !url || !shareable ? undefined : () => browserShare(message || url, url, title || message || url)
-    .then((res) => { notify && res in sharingMessage && notify(sharingMessage[res]) })
+  const handleShare = !url || !shareable ? undefined : () => { browserShare(message || url, url, title || message || url)
+    .then((res) => { notify && res in sharingMessage && notify(sharingMessage[res]) }) }
 
-  return !shareable ? null : (
+  return !handleShare ? null : (
     <a className={`link tooltip tooltip-primary ${className}`} onClick={handleShare} data-tip={tooltip}>
       <LinkIcon className={`${iconClass} h-auto fill-primary hover:fill-[color-mix(in_oklab,oklch(var(--p)),black_10%)] inline-block`} />
     </a>
