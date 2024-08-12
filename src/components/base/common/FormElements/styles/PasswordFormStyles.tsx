8d416bbe14c23fd8b3ffaf10f974fb000181072c
Nick C <bathtaters@gmail.com>
2024-08-12 02:33:19 -0400

    split off 'full' from props

--- src/components/base/common/FormElements/styles/PasswordFormStyles.tsx
+++ src/components/base/common/FormElements/styles/PasswordFormStyles.tsx
@@ -1,11 +1,11 @@
 import type { HTMLProps, ReactNode } from "react"
 
-export const FormWrapper = (props: HTMLProps<HTMLFormElement> & { full?: boolean }) => (
-    <form className={`w-full h-full flex flex-col ${props.full ? "items-center p-2" : ""} form-control`} {...props} />
+export const FormWrapper = ({ full, ...props}: HTMLProps<HTMLFormElement> & { full?: boolean }) => (
+    <form className={`w-full h-full flex flex-col ${full ? "items-center p-2" : ""} form-control`} {...props} />
 )
 
-export const LabelStyle = (props: HTMLProps<HTMLLabelElement> & { full?: boolean }) => (
-    <label className={`label label-text ${props.full ? "justify-center text-lg" : ""}`} {...props} />
+export const LabelStyle = ({ full, ...props}: HTMLProps<HTMLLabelElement> & { full?: boolean }) => (
+    <label className={`label label-text ${full ? "justify-center text-lg" : ""}`} {...props} />
 )
 
 export const BoxBtnWrapper = ({ full, children }: { full?: boolean, children?: ReactNode }) => (
