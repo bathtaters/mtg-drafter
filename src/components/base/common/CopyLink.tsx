f24befa458dde2e72fd119da01a2a642f054929e
Nick C <bathtaters@gmail.com>
2024-08-12 02:11:44 -0400

    fix for server-side hydration

--- src/components/base/common/CopyLink.tsx
+++ src/components/base/common/CopyLink.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import LinkIcon from "components/svgs/LinkIcon"
 import browserShare, { canShare } from "../libs/share"
 import { sharingMessage } from "assets/strings"
@@ -14,11 +15,13 @@ export type Props = {
 }
 
 export default function CopyLink({ url, message, title, notify, tooltip = "Copy Link", className = "", iconClass = "w-4" }: Props) {
+  const [shareable, setCanShare] = useState(false)
+  useEffect(() => { setCanShare(canShare()) }, [url]) // Needed for server-side hydration
 
-  const handleShare = !url || !canShare() ? undefined : () => browserShare(message || url, url, title || message || url)
-    .then((res) => notify && res in sharingMessage ? notify(sharingMessage[res]) : undefined)
+  const handleShare = !url || !shareable ? undefined : () => browserShare(message || url, url, title || message || url)
+    .then((res) => { notify && res in sharingMessage && notify(sharingMessage[res]) })
 
-  return !handleShare ? null : (
+  return !shareable ? null : (
     <a className={`link tooltip tooltip-primary ${className}`} onClick={handleShare} data-tip={tooltip}>
       <LinkIcon className={`${iconClass} h-auto fill-primary hover:fill-[color-mix(in_oklab,oklch(var(--p)),black_10%)] inline-block`} />
     </a>
