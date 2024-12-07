7c69b8e067aa6f7d9f883711dff54915e97d75ad
Nick C <bathtaters@gmail.com>
2024-12-07 02:20:28 -0500

    add autocomplete info to password form

--- src/components/base/common/FormElements/PasswordForm.tsx
+++ src/components/base/common/FormElements/PasswordForm.tsx
@@ -4,8 +4,8 @@ import { BoxBtnWrapper, ButtonStyle, FormWrapper, LabelStyle, MessageStyle, Pass
 
 
 export default function PasswordForm(
-    { label="Enter Password", fullPage, btnLabel="Submit", emptyBtn, message="", onSubmit, id="password", placeholder="" }:
-    { label?: string, btnLabel?: string, emptyBtn?: string, message?: string, id?: string, placeholder?: string, fullPage?: boolean, onSubmit?: (password: string) => Promise<void> | void }
+    { label="Enter Password", fullPage, btnLabel="Submit", emptyBtn, message="", onSubmit, id="password", placeholder="", isCreate=false }:
+    { label?: string, btnLabel?: string, emptyBtn?: string, message?: string, id?: string, placeholder?: string, fullPage?: boolean, isCreate?: boolean, onSubmit?: (password: string) => Promise<void> | void }
 ) {
     const [password, setPassword] = useState("")
 
@@ -22,7 +22,7 @@ export default function PasswordForm(
             <LabelStyle htmlFor={id} full={fullPage}>{label}</LabelStyle>
             { fullPage && <Spacer /> }
             <BoxBtnWrapper full={fullPage}>
-                <PasswordStyle id={id} value={password} placeholder={placeholder} onChange={handleChange} />
+                <PasswordStyle id={id} value={password} placeholder={placeholder} onChange={handleChange} autoComplete={isCreate ? 'new-password' : 'current-password'} />
                 { fullPage && <MessageStyle>{message}</MessageStyle> }
                 <ButtonStyle type="submit">{password || !emptyBtn ? btnLabel : emptyBtn}</ButtonStyle>
             </BoxBtnWrapper>
--- src/components/game/HostModal/HostModal.tsx
+++ src/components/game/HostModal/HostModal.tsx
@@ -67,7 +67,7 @@ export default function HostModal({
 
         <WatchContainer>
           <CopyLink {...shareWatch} url={watchUrl ? shareWatch.url(watchUrl) : undefined} notify={notify} />
-          <PasswordForm label="Live Watch Password" btnLabel="Set" emptyBtn="Clear" placeholder={watchUrl ? "••••••••" : ""} onSubmit={setWatchPw} />
+          <PasswordForm label="Live Watch Password" btnLabel="Set" emptyBtn="Clear" placeholder={watchUrl ? "••••••••" : ""} onSubmit={setWatchPw} isCreate={true} />
         </WatchContainer>
       </Loader>
     </ModalWrapper>
