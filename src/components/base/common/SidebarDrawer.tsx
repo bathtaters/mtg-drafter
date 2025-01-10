9ffd5960484e57b84a95cd48a3cfb7c8fc80b041
Nick C <bathtaters@gmail.com>
2025-01-09 22:59:28 -0500

    create generic SidebarDrawer component

---
+++ src/components/base/common/SidebarDrawer.tsx
@@ -0,0 +1,36 @@
+import type { MouseEventHandler, ReactNode } from "react"
+
+export type Props = {
+    isOpen: boolean,
+    overlayClick?: MouseEventHandler<HTMLLabelElement>,
+    className?: string,
+    sidebarClass?: string,
+    sidebarContent?: ReactNode,
+    children?: ReactNode,
+}
+
+export default function SidebarDrawer({
+    isOpen,
+    overlayClick,
+    className = "",
+    sidebarClass = "z-20",
+    sidebarContent,
+    children,
+}: Props) {
+  return (
+    <div className={`drawer${isOpen ? ' md:drawer-open' : ''} ${className}`}>
+        <input type="checkbox" className="drawer-toggle" checked={isOpen} />
+
+        <div className="drawer-content">
+            {children}
+        </div>
+
+        <div className={`drawer-side ${sidebarClass}`}>
+            {overlayClick &&
+                <label aria-label="close sidebar" className="drawer-overlay md:hidden" onClick={overlayClick} />
+            }
+            {sidebarContent}
+        </div>
+    </div>
+  )
+}
