04a5cc715520329b72d9d443e873c35c446b3a71
Nick C <bathtaters@gmail.com>
2025-01-09 23:19:23 -0500

    wrap Game page in PlayerSidebar

--- src/components/game/index/game.controller.ts
+++ src/components/game/index/game.controller.ts
@@ -1,6 +1,5 @@
 import type { PlayerFull, ServerProps } from 'types/game'
 import { useState } from 'react'
-import { useRouter } from 'next/router'
 import { useGameEmitters } from './services/socket.controller'
 import downloadDeck from './services/downloadDeck.controller'
 import { enableDropping } from 'assets/constants'
@@ -8,9 +7,7 @@ import useBasicGameController from './basic.controller'
 
 
 export default function useGameController(props: ServerProps) {
-  const router = useRouter()
-  const url = typeof router.query.url === 'string' ? router.query.url : '_INVALID'
-
+  const [sidebarVisible, setSidebar] = useState(false)
   const [landModal, setLandModal] = useState(false)
   const [hostModal, setHostModal] = useState(false)
   const [logModal,  setLogModal ] = useState(false)
@@ -31,7 +28,8 @@ export default function useGameController(props: ServerProps) {
   return {
     ...local,
     renamePlayer, setTitle, nextRound, pauseGame, pickCard, swapCard, setLands, setStatus, setWatchPw,
-
+    
+    sidebarVisible, setSidebar,
     landModal, hostModal, logModal, saveDeck,
     toggleLandModal, toggleHostModal, toggleLogModal,
     dropPlayer: enableDropping && local.player ? () => setStatus((local.player as PlayerFull).id, 'leave') : undefined,
