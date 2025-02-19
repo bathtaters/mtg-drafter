e491471a3d90b93a19da78362264db94b719b0ec
Nick C <bathtaters@gmail.com>
2025-02-19 01:07:49 -0500

    add placeholder prop, allow setting props on <select> tag as well as className of parent wrapper

--- src/components/base/common/FormElements/Selector.tsx
+++ src/components/base/common/FormElements/Selector.tsx
@@ -1,17 +1,25 @@
-import { ReactNode, Children, ReactElement, Component } from "react"
+import type { ReactNode, ReactElement, HTMLProps } from "react"
+import { Children } from "react"
 
 export type Props<ID extends string|number> = {
-  label?: string,
-  selected: ID,
-  setSelected: (key: string) => void,
-  className?: string,
+  selected?: ID,
+  setSelected: (key?: ID) => void,
+  placeholder?: ReactNode,
   children: ReactElement<any>[],
-}
+  wrapperClass?: string,
+} & Omit<HTMLProps<HTMLSelectElement>, 'children'|'selected'|'placeholder'>
 
-export default function Selector<ID extends string|number = number>({ label, selected, setSelected, className, children }: Props<ID>) {
+const PLACEHOLDER = "__PLACEHOLDER__"
+
+export default function Selector<ID extends string|number = number>({ label, selected, setSelected, placeholder, className, wrapperClass, children, ...props }: Props<ID>) {
   return (
-    <div className={`${label ? 'tooltip' : ''} tooltip-secondary`} data-tip={label}>
-      <select className={`select ${className || ''}`} value={selected} onChange={(ev) => setSelected(ev.target.value)}>
+    <div className={`${label ? 'tooltip' : ''} tooltip-secondary ${wrapperClass ?? ''}`} data-tip={label}>
+      <select {...props}
+        value={selected ?? PLACEHOLDER}
+        onChange={(ev) => setSelected(ev.target.value === PLACEHOLDER ? undefined : ev.target.value as ID)}
+        className={`select ${className || ''}${selected == null ? ' italic opacity-80' : ''}`}
+      >
+        {placeholder && <option value={PLACEHOLDER} disabled={true}>{placeholder}</option>}
         {Children.map(children, (child, idx) => <option key={child.key ?? idx} value={child.key ?? idx}>{child}</option>)}
       </select>
     </div>
