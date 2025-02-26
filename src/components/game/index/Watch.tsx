681e35c801216863ffd34b01b47a75896824f295
Nick C <bathtaters@gmail.com>
2025-02-25 23:00:52 -0500

    add tabs with LogToolbar to Watch page

--- src/components/game/index/Watch.tsx
+++ src/components/game/index/Watch.tsx
@@ -1,21 +1,35 @@
-import type { ServerProps } from 'types/game'
+import { type ServerProps, WatcherTabs } from 'types/game'
 import GameLogHeader from 'components/game/GameLog/GameLogHeader'
 import GameLogWatch from 'components/game/GameLog/GameLogWatch'
+import PackViewer from '../PackViewer/PackViewer'
+import ContainerTabs from '../GameBody/ContainerTabs'
+import LogToolbar from '../GameLog/LogToolbar/LogToolbar'
+import CardToolbar from '../CardToolbar/CardToolbar'
 import PlayerSidebar from '../PlayerSidebar/PlayerSidebar'
 import Overlay from 'components/base/common/Overlay'
 import Spinner from 'components/base/common/Spinner'
 import Loader from 'components/base/Loader'
 import Footer from 'components/base/Footer'
 import { BodyWrapperStyle, SetPageTitle } from 'components/base/styles/AppStyles'
+import { GameBodyHeader, GameBodyWrapper } from '../GameBody/GameBodyStyles'
+import { TabToolbarWrapper } from '../GameLog/LogToolbar/LogToolbarStyles'
 import useBasicGameController from './basic.controller'
+import useLogWatch from '../GameLog/logWatch.controller'
+import { useTabController } from '../GameBody/pick.controller'
 import { banMsg } from 'assets/strings'
 
 
 export default function Watch(props: ServerProps) {
   const {
-    game, players, packs, socket, sessionId, maxPackSize, holding, gameLog, isBanned,
-    sidebarVisible, setSidebar, loadingAll, setLoadingAll, newError, reload,
+    game, players, packs, maxPackSize, holding, gameLog, isBanned,
+    sidebarVisible, setSidebar, loadingAll, newToast, ...basicProps
   } = useBasicGameController(props)
+  
+  const {
+    selectedTab, selectTab, cardOptions, setCardOptions, pickInfo
+  } = useTabController<WatcherTabs>(WatcherTabs.log, game, players, false)
+
+  const watchProps = useLogWatch({ gameLog, game, sidebarVisible, setSidebar, ...basicProps })
 
   return (<>
     <SetPageTitle title={game?.name || ""} />
@@ -28,20 +42,25 @@ export default function Watch(props: ServerProps) {
       
       <BodyWrapperStyle>
         <Loader data={game || 404} message={props.error || (isBanned && banMsg)}>
-          <GameLogWatch
-            game={game}
-            players={players}
-            packs={packs}
-            socket={socket}
-            log={gameLog}
-            sessionId={sessionId}
-            gameEnded={true}
-            reload={reload}
-            setLoading={setLoadingAll}
-            sidebarVisible={sidebarVisible}
-            setSidebar={setSidebar}
-            newError={newError}
-          />
+          <GameBodyWrapper className={cardOptions.width}>
+              <GameBodyHeader>
+                <ContainerTabs tabs={WatcherTabs} selectedTab={selectedTab} selectTab={selectTab} hideTabs={[WatcherTabs.join]} />
+
+                { selectedTab === WatcherTabs.pack ?
+                  <CardToolbar setCardOptions={setCardOptions} clickReload={basicProps.reload} notify={newToast} />
+                  :
+                  <TabToolbarWrapper clickReload={basicProps.reload}>
+                    <LogToolbar log={gameLog} players={players} gameEnded={true} logout={watchProps.logout} sidebarVisible={sidebarVisible} setSidebar={setSidebar} />
+                  </TabToolbarWrapper>
+                }
+              </GameBodyHeader>
+            
+              { selectedTab === WatcherTabs.pack ?
+                <PackViewer packs={packs} cardOptions={cardOptions} players={players} game={game} pickInfo={pickInfo} />
+                :
+                <GameLogWatch players={players} packs={packs} log={gameLog} disabled={!game?.watchKey} {...watchProps} />
+              }
+            </GameBodyWrapper>
         </Loader>
       </BodyWrapperStyle>
       
