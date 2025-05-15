443bb35b9d876db926c0b47fd253baeb514167a4
Nick C <bathtaters@gmail.com>
2025-05-14 23:30:37 -0400

    allow changing file extension

--- src/components/base/libs/download.ts
+++ src/components/base/libs/download.ts
@@ -1,9 +1,9 @@
-export default function downloadTextFile(filename: string, text: string) {
+export default function downloadTextFile(filename: string, text: string, ext='.txt') {
   const element = document.createElement("a")
   const file = new Blob([text], {type: 'text/plain'})
 
   element.href = URL.createObjectURL(file)
-  element.download = `${filename}.txt`
+  element.download = `${filename}${ext}`
   
   document.body.appendChild(element)
   element.click()
