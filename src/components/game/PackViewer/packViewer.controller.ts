a34f3f3593b0c0760a8549efa5eaa72ad01837cc
Nick C <bathtaters@gmail.com>
2025-05-24 00:24:04 -0400

    fix missing pickInfo in larger games due to paginated API

--- src/components/game/PackViewer/packViewer.controller.ts
+++ src/components/game/PackViewer/packViewer.controller.ts
@@ -1,11 +1,14 @@
-import type { BasicPlayer, Game, LogFull, PackFull, PickInfo, Player } from "types/game"
+import type { BasicPlayer, Game, LogEntryFull, PackFull, PickInfo, Player } from "types/game"
 import type { GameClient } from "backend/controllers/game.socket.d"
 import type { ErrorAlert } from "components/base/common/Alerts/alerts.d"
 import { useCallback, useEffect, useMemo, useState } from "react"
 import { TabLabels } from "types/game"
-import { fetcher } from "components/base/libs/fetch"
-import { getDeck, getPack, logQryFilter, logToPickInfo, sortByPickOrder } from "./packViewer.utils"
+import { paginatedFetcher } from "components/base/libs/fetch"
+import { getDeck, getPack, logToPickInfo, sortByPickOrder } from "./packViewer.utils"
 import { viewAuthError } from "assets/strings"
+import { logFetchOptions } from "assets/constants"
+
+const size = logFetchOptions.maxSize, params = { filter: { actions: ["pick"] } }
 
 const formatErr = (message: string): ErrorAlert => ({ message, title: 'Cannot view cards', theme: 'warning' })
 
@@ -21,18 +24,16 @@ export function usePickInfo(game?: Partial<Game>, players?: BasicPlayer[], skip?
         if (!game?.url) return setData(undefined)
         
         setLoading(true)
-        const res = await fetcher<LogFull>(`/api/game/${game.url}/log?${logQryFilter}`)
-        if (res.status === 204) {
-            setData(undefined) // Empty log
-            setLoading(false)            
-        } else if (res.status !== 200 || res.error || res.data?.total == null) {
+        
+        const res = await paginatedFetcher<LogEntryFull>(`/api/game/${game.url}/log`, { size, params, key: 'log' })
+        setLoading(false)
+        if (typeof res === 'number') {
             setData(undefined)
-            setError(`Error <${res.status}> while fetching pick order.`)
-            setLoading(false)
-        } else {
-            setData(logToPickInfo(res.data.log, players))
-            setLoading(false)
+            setError(`Error <${res}> while fetching pick order.`)
+            return
         }
+        setData(logToPickInfo(res, players))
+
     }, [game?.url, players])
 
     useEffect(() => { !skip && updatePicks() }, [updatePicks, skip])
